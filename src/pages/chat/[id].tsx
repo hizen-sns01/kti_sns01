8a1e0f5bd339141f48143c8a2b2456246a8c15aa
seong-geon-choi <sgchoisg@gmail.com>
2025-11-07 09:11:12 +0900

    채팅방 제목 위치 수정

--- src/pages/chat/[id].tsx
+++ src/pages/chat/[id].tsx
@@ -464,7 +464,7 @@ const ChatroomPage: React.FC = () => {
     <div className="flex flex-col h-[calc(100vh-9rem)]">
       {/* Chatroom Header with Hamburger Menu */}
       <div className="flex justify-between items-center p-4 border-b bg-white relative">
-        <Link href="/chat">
+        <Link href="/chat" className="absolute left-4 top-1/2 transform -translate-y-1/2 z-30">
           <a className="text-blue-500 hover:underline flex items-center">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 mr-1">
               <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
