764145e130ff99d0b78d5a1347a1abd3253a94e2
hizen-sns01 <hizen.sns01@gmail.com>
2025-11-14 18:05:25 +0900

    에러 수정

--- src/components/AllPopularTopics.tsx
+++ src/components/AllPopularTopics.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { supabase } from '../supabaseClient';
 import Link from 'next/link';
+import { useUserProfile } from '../hooks/useUserProfile';
 
 // --- Interfaces ---
 interface Comment {
